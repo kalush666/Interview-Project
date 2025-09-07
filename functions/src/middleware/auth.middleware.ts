bc157bf9c9341a7f15e179ed94007f75508079f5
kalush666 <jonathan.kalush@gmail.com>
2025-09-07 20:19:07 +0300

    auth middle ware

---
+++ functions/src/middleware/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Response } from "express";
+import { AuthRequest } from "../types/auth.types";
+import { Auth, STATUS_MESSAGES } from "../constants";
+import { fireBaseAdmin } from "../config/firebase";
+import { DecodedIdToken } from "firebase-admin/auth";
+
+export const authMiddleware = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith(Auth.BEARER_PREFIX)) {
+    res.status(STATUS_MESSAGES.HTTP_STATUS.UNAUTHORIZED).json({
+      success: false,
+      message: STATUS_MESSAGES.ERROR_MESSAGES.UNAUTHORIZED,
+    });
+    return;
+  }
+  const token: string = authHeader.split(" ")[Auth.TOKEN_PLACEMENT];
+  try {
+    const decodedToken: DecodedIdToken = await fireBaseAdmin
+      .getAuth()
+      .verifyIdToken(token);
+    req.user = {
+      uid: decodedToken.uid,
+      email: decodedToken.email || "",
+    };
+  } catch (error) {
+    res.status(STATUS_MESSAGES.HTTP_STATUS.UNAUTHORIZED).json({
+      success: false,
+      message: STATUS_MESSAGES.ERROR_MESSAGES.UNAUTHORIZED,
+    });
+  }
+};
--- functions/src/types/auth.types.ts
+++ functions/src/types/auth.types.ts
@@ -2,8 +2,8 @@ import { Request } from "express";
 
 export interface AuthRequest extends Request {
   user?: {
-    id: string;
-    email: string;
+    uid: string;
+    email?: string;
   };
 }
 
