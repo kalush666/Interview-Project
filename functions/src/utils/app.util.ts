922d037081b10de1087258e80b9a907e93e4eea3
kalush666 <jonathan.kalush@gmail.com>
2025-09-08 18:08:18 +0300

    import update

--- functions/src/utils/app.util.ts
+++ functions/src/utils/app.util.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
-import { Configuration } from "../constants";
 import { ChatService } from "../services/chat.service";
 
 export function createApp() {
   const app = express();
   app.use(helmet());
-  app.use(cors(Configuration.DEFAULT_CORS_CONFIG));
+  app.use(cors({ origin: true }));
   app.use(express.json());
-  app.use(express.urlencoded(Configuration.DEFAULT_EXPRESS_URL_ENCODED_CONFIG));
+  app.use(express.urlencoded({ extended: true }));
 
   const chatService = new ChatService();
   chatService.initializeDefaultRoom().catch(console.error);
